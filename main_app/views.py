cae530d60aabd554ee57669a49ac4060f9cfb4bf
cagdaskalsen <cagdaskalsen@hotmail.com>
2023-01-31 21:07:56 -0500

    Home page set up

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
-
+from django.views import View
+from django.http import HttpResponse
+from django.views.generic.base import TemplateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.detail import DetailView
+# from . models import Product
 # Create your views here.
+
+
+class Home(TemplateView):
+    template_name = 'home.html'
+
+
+class About(TemplateView):
+
+    template_name = "about.html"
