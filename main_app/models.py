ef935711e53a90a4d8e348352ec0e637b8c00593
cagdaskalsen <cagdaskalsen@hotmail.com>
2023-02-01 01:26:41 -0500

    Products page updated

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Customers(models.Model):
+    first_name = models.CharField(max_length=250)
+    last_name = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.first_name
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.title
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=250)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to="products")
+    price = models.FloatField()
+    description = models.TextField
+
+    def __str__(self):
+        return self.title
--- main_app/views.py
+++ main_app/views.py
@@ -4,12 +4,17 @@ from django.http import HttpResponse
 from django.views.generic.base import TemplateView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic.detail import DetailView
-# from . models import Product
+from . models import Product
 # Create your views here.
 
 
 class Home(TemplateView):
     template_name = 'home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['products'] = Product.objects.all()
+        return context
 
 
 class About(TemplateView):
